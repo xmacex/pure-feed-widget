0c00883426ec03d6664e528fe6a6d837d7133269
Mace Ojala <maco@itu.dk>
2019-05-01 17:33:28 +0200

    Parametrized the API endpoint URL and organization UUID. Closes #3, actually since this whole merge was started and merged to master already.

--- PureWsRestRendering.php
+++ PureWsRestRendering.php
@@ -16,22 +16,26 @@ class PureWsRestRendering
      * @param SimpleXML $xmldata If given, loads this data instead
      * @param string $rendering  Rendering style
      */
-    function __construct($path, $xmldata=NULL, $noitems=5, $orderby='publicationDate', $rendering='vancouver')
+    function __construct($endpoint, $org=NULL, $xmldata=NULL, $noitems=5, $orderby='publicationDate', $rendering='vancouver', $orgagg='RecursiveContentValueAggregator')
     {
+        $this->org = $org;
         $this->noitems = $noitems;
         $this->orderby = $orderby;
         $this->rendering = $rendering;
+        $this->orgagg = $orgagg;
         $this->publications = [];
 
         if ($xmldata) {
             $xml = $xmldata;
         } else {
-            $params = ['window.size' => $this->noitems,
+            $params = ['associatedOrganisationUuids.uuid' => $this->org,
+                       'window.size' => $this->noitems,
                        'orderBy.property' => $this->orderby,
                        'rendering' => $this->rendering,
+                       'associatedOrganizationAggregationStrategy' => $this->orgagg,
                        'linkingStrategy' => 'portalLinkingStrategy',
                        'locale' => 'en_GB'];
-            $url = $path . "&" . http_build_query($params);
+            $url = $endpoint . "/publication" . "?" . http_build_query($params);
             $xml = simplexml_load_file($url);
         }
 
