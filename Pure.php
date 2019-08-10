b08b6e76130356e3ae9a65fcca708a94b08946a3
Mace Ojala <maco@itu.dk>
2019-08-10 20:00:48 +0200

    Ok refactoring more or less done, now a much clearer ontology of representation.

--- Pure.php
+++ Pure.php
@@ -1,8 +1,6 @@
 <?php
 /**
  * An abstraction over Pure, to simulate the data source
- *
- * The serverside does the rendering.
  */
 
 require_once('Publication.php');
@@ -10,94 +8,95 @@ require_once('Publication.php');
 class Pure
 {
     /**
-     * Constructs a data source
+     * Constructs a data source.
      *
      * @param string $path       URL to grab data from
      * @param SimpleXML $xmldata If given, loads this data instead
      * @param string $rendering  Rendering style
      */
-    function __construct($endpoint, $apikey, $org=NULL, $xmldata=NULL, $noitems=5, $orderby='publicationYear', $rendering='vancouver', $orgagg='RecursiveContentValueAggregator')
+    function __construct(string $url, string $apikey)
     {
-        $this->org = $org;
-        $this->noitems = $noitems;
-        $this->orderby = $orderby;
-        $this->rendering = $rendering;
-        $this->orgagg = $orgagg;
-
-        $this->publications = [];
-
-        if ($xmldata) {
-            $xml = $xmldata;
-        } else {
-            $params = [
-                'size' => $this->noitems,
-                'order' => '-publicationYear',
-                'rendering' => $this->rendering,
-                'associatedOrganizationAggregationStrategy' => $this->orgagg,
-                'linkingStrategy' => 'portalLinkingStrategy',
-                'publicationStatus' => '/dk/atira/pure/researchoutput/status/published',
-                'locale' => 'en_GB',
-                'apiKey' => $apikey
-            ];
-            // Using HTTP GET
-            // $url = $endpoint . "/organisational-units/" . $this->org . "/research-outputs" . "?" . http_build_query($params);
-            // $xml = simplexml_load_file($url);
-
-            // Using HTTP POST
-            $url = $endpoint . "/research-outputs/?apiKey=" . $apikey;
-            $xml = $this->get_publications($url);
-        }
-
-        foreach($xml->xpath('//result/items//renderings/rendering') as $item) {
-            $this->publications[] = new Publication($item, $this->rendering);
-        }
+        $this->url = $url;
+        $this->apikey = $apikey;
     }
 
-    function get_publications($url)
+    /**
+     * Get research output from the Pure.
+     *
+     * @param string $org       Organization to filter
+     * @param int    $size      Number of items to get
+     * @param string $order     Sorting order, supported by the API
+     * @param string $rendering Rendering type
+     *
+     * @return array Of publications
+     *
+     */
+    function get_research_outputs(string $org=NULL, int $size=5, string $rendering='vancouver')
     {
+        $endpoint = 'research-outputs';
+        $order = '-publicationYear'; // Not exposed as a parameter
+
+        $research_outputs = [];
+
+        // Parameters in an array would be more native than making XML things
         /*
-        $query = '<?xml version="1.0"?>
-        <researchOutputsQuery>
-            <size>3</size>
-            <linkingStrategy>portalLinkingStrategy</linkingStrategy>
-            <renderings>
-                <rendering>apa</rendering>
-            </renderings>
-            <orderings>
-                <ordering>-publicationYear</ordering>
-            </orderings>
-            <publicationStatuses>
-                <publicationStatus>/dk/atira/pure/researchoutput/status/published</publicationStatus>
-            </publicationStatuses>
-            <forOrganisationalUnits>
-            <uuids>
-                <uuid>ca087d09-fd99-4c42-8180-9799383c072e</uuid>
-            </uuids>
-            </forOrganisationalUnits>
-        </researchOutputsQuery>';
+        $params = [
+            'size' => $size,
+            'order' => $order,
+            'rendering' => $rendering,
+            // 'associatedOrganizationAggregationStrategy' => $qparam->orgagg,
+            'linkingStrategy' => 'portalLinkingStrategy',
+            'publicationStatus' => '/dk/atira/pure/researchoutput/status/published',
+            'locale' => 'en_GB',
+            'apiKey' => $this->apikey
+        ];
+        */
+
+        /*
+        // Using HTTP GET
+        $url = $endpoint . "/organisational-units/" . $this->org . "/research-outputs" . "?" . http_build_query($params);
+        $xml = simplexml_load_file($url);
         */
 
         $query = new SimpleXMLElement('<researchOutputsQuery/>');
-        $query->addChild('size', $this->noitems);
+        $query->addChild('size', $size);
         $query->addChild('linkingStrategy', 'portalLinkingStrategy'); // This needs to be before renderings
-        $query->addChild('renderings')->addChild('rendering', $this->rendering);
+        $query->addChild('renderings')->addChild('rendering', $rendering);
         // $query->addChild('orderings')->addChild('ordering', '-publicationYear');
-        $query->addChild('orderings')->addChild('ordering', $this->orderby);
+        $query->addChild('orderings')->addChild('ordering', $order);
         $query->addChild('publicationStatuses')->addChild('publicationStatus', '/dk/atira/pure/researchoutput/status/published');
         $query->addChild('forOrganisationalUnits')->addChild('uuids')->addChild('uuid', 'ca087d09-fd99-4c42-8180-9799383c072e');
 
-        $curl = curl_init($url);
+        $xml = $this->query($endpoint, $query);
+
+        foreach($xml->xpath('//result/items//renderings/rendering') as $item) {
+            $research_outputs[] = new Publication($item, $rendering);
+        }
+        return $research_outputs;
+    }
+
+    /**
+     * Query the API
+     *
+     * @param  string $endpoint        API endpoint, ie resource type
+     * @param  SimpleXMLElement $query Query parameters as an SimpleXMLElement
+     *
+     * @return string $xml representation of the response
+     */
+    function query(string $endpoint, SimpleXMLElement $query)
+    {
+        $curl = curl_init($this->url . '/' . $endpoint . '?' . http_build_query(['apiKey' => $this->apikey]));
         curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/xml"));
         curl_setopt($curl, CURLOPT_POST, true);
         // curl_setopt($curl, CURLOPT_POSTFIELDS, $query);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $query->asXML());
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        // var_dump(curl_getinfo($curl));
         $result = curl_exec($curl);
 
         if(curl_errno($curl)) {
             throw new Exception(curl_error($curl));
         }
-
         curl_close($curl);
 
         $xml = simplexml_load_string($result);
