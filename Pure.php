3c351c5b8595d6c6f24bac62beb2f33a709af42b
Mace Ojala <maco@itu.dk>
2019-08-22 17:59:03 +0000

    Coding style changes using PHPCS and the WordPress coding style rules and other rules as feedback.
    git-svn-id: http://plugins.svn.wordpress.org/pure-feed-widget/trunk@2143985 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Pure.php
+++ Pure.php
@@ -3,104 +3,100 @@
  * An abstraction over Pure, to simulate the data source
  */
 
-require_once('Publication.php');
+require_once 'Publication.php';
 
-class Pure
-{
-    /**
-     * Constructs a data source.
-     *
-     * @param string $path       URL to grab data from
-     * @param SimpleXML $xmldata If given, loads this data instead
-     * @param string $rendering  Rendering style
-     */
-    function __construct(string $url, string $apikey)
-    {
-        $this->url = $url;
-        $this->apikey = $apikey;
-    }
+/**
+ * A Pure API representation.
+ */
+class Pure {
+	/**
+	 * Constructs a data source.
+	 *
+	 * @param string $url     URL to grab data from.
+	 * @param string $apikey  Rendering style.
+	 */
+	public function __construct( string $url, string $apikey ) {
+		$this->url    = $url;
+		$this->apikey = $apikey;
+	}
 
-    /**
-     * Get research output from the Pure.
-     *
-     * @param string $org       Organization to filter
-     * @param int    $size      Number of items to get
-     * @param string $order     Sorting order, supported by the API
-     * @param string $rendering Rendering type
-     *
-     * @return array Of publications
-     *
-     */
-    function get_research_outputs(string $org=NULL, int $size=5, string $rendering='vancouver')
-    {
-        $endpoint = 'research-outputs';
-        $order = '-publicationYear'; // Not exposed as a parameter
+	/**
+	 * Get research output from the Pure.
+	 *
+	 * @param string $org        Organization to filter.
+	 * @param int    $size       Number of items to get.
+	 * @param string $rendering  Rendering type.
+	 *
+	 * @return array Of publications
+	 */
+	public function get_research_outputs( string $org = null, int $size = 5, string $rendering = 'vancouver' ) {
+		$endpoint = 'research-outputs';
+		$order    = '-publicationYear'; // Not exposed as a parameter.
 
-        $research_outputs = [];
+		$research_outputs = [];
 
-        // Parameters in an array would be more native than making XML things
-        /*
-        $params = [
-            'size' => $size,
-            'order' => $order,
-            'rendering' => $rendering,
-            // 'associatedOrganizationAggregationStrategy' => $qparam->orgagg,
-            'linkingStrategy' => 'portalLinkingStrategy',
-            'publicationStatus' => '/dk/atira/pure/researchoutput/status/published',
-            'locale' => 'en_GB',
-            'apiKey' => $this->apikey
-        ];
-        */
+		// Parameters in an array would be more native than making XML things.
 
-        /*
-        // Using HTTP GET
-        $url = $endpoint . "/organisational-units/" . $this->org . "/research-outputs" . "?" . http_build_query($params);
-        $xml = simplexml_load_file($url);
-        */
+		/*
+		$params = [
+			'size' => $size,
+			'order' => $order,
+			'rendering' => $rendering,
+			// 'associatedOrganizationAggregationStrategy' => $qparam->orgagg,
+			'linkingStrategy' => 'portalLinkingStrategy',
+			'publicationStatus' => '/dk/atira/pure/researchoutput/status/published',
+			'locale' => 'en_GB',
+			'apiKey' => $this->apikey
+		];
+		*/
 
-        $query = new SimpleXMLElement('<researchOutputsQuery/>');
-        $query->addChild('size', $size);
-        $query->addChild('linkingStrategy', 'portalLinkingStrategy'); // This needs to near the top
-        $query->addChild('locales')->addChild('locale', 'en_GB');
-        $query->addChild('fallbackLocales')->addChild('fallbackLocale', 'en_GB');
-        $query->addChild('renderings')->addChild('rendering', $rendering);
-        // $query->addChild('orderings')->addChild('ordering', '-publicationYear');
-        $query->addChild('orderings')->addChild('ordering', $order);
-        $query->addChild('publicationStatuses')->addChild('publicationStatus', '/dk/atira/pure/researchoutput/status/published');
-        $query->addChild('forOrganisationalUnits')->addChild('uuids')->addChild('uuid', $org);
+		/*
+		// Using HTTP GET
+		$url = $endpoint . "/organisational-units/" . $this->org . "/research-outputs" . "?" . http_build_query($params);
+		$xml = simplexml_load_file($url);
+		*/
 
-        $xml = $this->query($endpoint, $query);
+		$query = new SimpleXMLElement( '<researchOutputsQuery/>' );
+		$query->addChild( 'size', $size );
+		$query->addChild( 'linkingStrategy', 'portalLinkingStrategy' ); // This needs to near the top.
+		$query->addChild( 'locales' )->addChild( 'locale', 'en_GB' );
+		$query->addChild( 'fallbackLocales' )->addChild( 'fallbackLocale', 'en_GB' );
+		$query->addChild( 'renderings' )->addChild( 'rendering', $rendering );
+		// $query->addChild( 'orderings')->addChild('ordering', '-publicationYear');
+		$query->addChild( 'orderings' )->addChild( 'ordering', $order );
+		$query->addChild( 'publicationStatuses' )->addChild( 'publicationStatus', '/dk/atira/pure/researchoutput/status/published' );
+		$query->addChild( 'forOrganisationalUnits' )->addChild( 'uuids' )->addChild( 'uuid', $org );
 
-        foreach($xml->xpath('//result/items//renderings/rendering') as $item) {
-            $research_outputs[] = new Publication($item, $rendering);
-        }
-        return $research_outputs;
-    }
+		$xml = $this->query( $endpoint, $query );
 
-    /**
-     * Query the API
-     *
-     * @param  string $endpoint        API endpoint, ie resource type
-     * @param  SimpleXMLElement $query Query parameters as an SimpleXMLElement
-     *
-     * @return string $xml representation of the response
-     */
-    function query(string $endpoint, SimpleXMLElement $query)
-    {
-        $curl = curl_init($this->url . '/' . $endpoint . '?' . http_build_query(['apiKey' => $this->apikey]));
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/xml"));
-        curl_setopt($curl, CURLOPT_POST, true);
-        // curl_setopt($curl, CURLOPT_POSTFIELDS, $query);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $query->asXML());
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        $result = curl_exec($curl);
+		foreach ( $xml->xpath( '//result/items//renderings/rendering' ) as $item ) {
+			$research_outputs[] = new Publication( $item, $rendering );
+		}
+		return $research_outputs;
+	}
 
-        if(curl_errno($curl)) {
-            throw new Exception(curl_error($curl));
-        }
-        curl_close($curl);
+	/**
+	 * Query the API
+	 *
+	 * @param string           $endpoint  API endpoint, ie resource type.
+	 * @param SimpleXMLElement $query     Query parameters as an SimpleXMLElement.
+	 * @return string          $xml       Representation of the response.
+	 * @throws Exception                  Stuff went wrong.
+	 */
+	private function query( string $endpoint, SimpleXMLElement $query ) {
+		$curl = curl_init( $this->url . '/' . $endpoint . '?' . http_build_query( [ 'apiKey' => $this->apikey ] ) );
+		curl_setopt( $curl, CURLOPT_HTTPHEADER, array( 'Content-Type: application/xml' ) );
+		curl_setopt( $curl, CURLOPT_POST, true );
+		curl_setopt( $curl, CURLOPT_POSTFIELDS, $query->asXML() );
+		curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+		$result = curl_exec( $curl );
 
-        $xml = simplexml_load_string($result);
-        return $xml;
-    }
+		if ( curl_errno( $curl ) ) {
+			throw new Exception( curl_error( $curl ) );
+		}
+		curl_close( $curl );
+
+		$xml = simplexml_load_string( $result );
+		return $xml;
+	}
 }
