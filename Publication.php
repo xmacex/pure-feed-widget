15342ffea625b5dfc8319e0f97d6f262d189b273
xmacex <xmacex@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2019-08-22 17:54:12 +0000

    An incomplete publication title of host publication inference thing. Test setup needs to be refactored to test just one type of contribution at a time.
    git-svn-id: http://plugins.svn.wordpress.org/pure-feed-widget/trunk@2143951 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Publication.php
+++ Publication.php
@@ -17,7 +17,40 @@ class Publication
         $this->date = (integer)$elem->xpath('stabl:publicationDate/core:year')[0];
         $this->title = (string)$elem->xpath('stabl:title')[0];
         $this->link = (string)$elem->xpath('core:portalUrl')[0];
-        $this->publication = '<span style="color: red";>Publication</span>'; // FIXME
+        $this->publication = $this->retrieve_hosttitle($elem);
+    }
+
+    /**
+     * Retrieve publication for an item.
+     *
+     * Figuring out what to have for publication is maybe not exactly
+     * trivial, thus a function for it. Might instead set properties
+     * directly.
+     *
+     * @param  SimpleXmlElement $elem
+     * @return string           host publication title
+     */
+    private function retrieve_hosttitle($elem)
+    {
+        $pubtype = (string)$elem->xpath('@xsi:type')[0];
+        
+        switch($pubtype) {
+        case 'stab:ContributionToBookAnthologyType':
+            $host = (string)$elem->xpath('stabl:hostPublicationTitle')[0];
+            break;
+        case 'stab:ContributionToJournalType':
+            $host = (string)$elem->xpath('stabl:journal/journal-template:title/extensions-core:string')[0];
+            break;
+        /*
+        case 'stab:ContributionToConferenceType':
+            $host = (string)$elem->xpath('');
+            break;
+            */
+        default:
+            $host = '<span style="color: red;">unknown</span>';
+        }
+
+        return $host;
     }
 
     public function __toString()
