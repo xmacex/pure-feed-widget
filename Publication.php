a93f583851bbb94e0748930b802ca0682e0bd72f
xmacex <xmacex@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2019-08-22 17:59:03 +0000

    Coding style changes using PHPCS and the WordPress coding style rules and other rules as feedback.
    git-svn-id: http://plugins.svn.wordpress.org/pure-feed-widget/trunk@2143985 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Publication.php
+++ Publication.php
@@ -2,48 +2,45 @@
 /**
  * A publication
  */
-class Publication
-{
-    /**
-     * Constructor.
-     *
-     * If $rendering is given, then $data is already rendered on
-     * serverside or otherwise. If it is not given, then rendering is
-     * left to us to do.
-     *
-     * @param string $data      Bibliographical data
-     * @param string $rendering Prerendering style, e.g. apa, vancouver
-     */
-    public function __construct(string $data, string $rendering=NULL)
-    {
-        $this->rendering = $rendering;
-        if($this->rendering) {
-            $this->rendered = $data;
-        } else {
-            throw new Exception('Parsing of raw API data not implemented yet');
-        }
-    }
+class Publication {
+	/**
+	 * Constructor.
+	 *
+	 * If $rendering is given, then $data is already rendered on
+	 * serverside or otherwise. If it is not given, then rendering is
+	 * left to us to do.
+	 *
+	 * @param string $data      Bibliographical data.
+	 * @param string $rendering Prerendering style, e.g. apa, vancouver.
+	 * @throws Exception        If there are issues.
+	 */
+	public function __construct( string $data, string $rendering = null ) {
+		$this->rendering = $rendering;
+		if ( $this->rendering ) {
+			$this->rendered = $data;
+		} else {
+			throw new Exception( 'Parsing of raw API data not implemented yet' );
+		}
+	}
 
-    /**
-     * Render as string.
-     *
-     * @return string String representation.
-     */
-    public function __toString()
-    {
-        return (string)$this->rendered->asXML();
-    }
+	/**
+	 * Render as string.
+	 *
+	 * @return string String representation.
+	 */
+	public function __toString() {
+		return (string) $this->rendered->asXML();
+	}
 
-    /**
-     * Render as HTML.
-     *
-     * @return string HTML representation.
-     */
-    public function asHtml()
-    {
-        $output = "<li class='item'>";
-        $output .= (string)$this->rendered;
-        $output .= "</li>";
-        return $output;
-    }
+	/**
+	 * Render as HTML.
+	 *
+	 * @return string HTML representation.
+	 */
+	public function asHtml() {
+		$output  = "<li class='item'>";
+		$output .= (string) $this->rendered;
+		$output .= '</li>';
+		return $output;
+	}
 }
