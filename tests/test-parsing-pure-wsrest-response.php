f60766ee5947f3689ea82f9ea7996a17d8fed86f
Mace Ojala <maco@itu.dk>
2019-08-22 17:53:38 +0000

    Ok, moved now. LOL a stray semicolon was quite a hunt. Looks relatively functional now, considering move from Pure RSS feeds to WS REST thing.
    git-svn-id: http://plugins.svn.wordpress.org/pure-feed-widget/trunk@2143947 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tests/test-parsing-pure-wsrest-response.php
+++ tests/test-parsing-pure-wsrest-response.php
@@ -5,21 +5,18 @@
  * WS REST API output.
  */
 
-// const FEEDURL = 'https://pure.itu.dk/ws/rest/publication?associatedOrganisationUuids.uuid=cf9b4e6a-e1ad-41e3-9475-7679abe7131b&window.size=5&associatedOrganisationAggregationStrategy=RecursiveContentValueAggregator';
-const FEEDURL = 'tests/publications-via-wsrest.xml';
-
 class TestParsingPureWsRestResponse extends WP_UnitTestCase
 {
     function test_retrieving_feed_should_succeed_on_valid_url()
     {
-        $xml = simplexml_load_file(FEEDURL);
+        $xml = simplexml_load_file(WSRESTFILE);
 
         $this->assertInstanceOf(SimpleXMLElement::class, $xml);
     }
 
     function test_parsed_response_shoult_contain_five_publications()
     {
-        $xml = simplexml_load_file(FEEDURL);
+        $xml = simplexml_load_file(WSRESTFILE);
 
         $results = $xml->xpath('//core:result/core:content');
         $this->assertCount(5, $results);
@@ -27,7 +24,7 @@ class TestParsingPureWsRestResponse extends WP_UnitTestCase
 
     function test_parsing_of_titles_should_work_from_valid_data()
     {
-        $xml = simplexml_load_file(FEEDURL);
+        $xml = simplexml_load_file(WSRESTFILE);
 
         $this->assertEquals("Full Synopsis of EVA Copenhagen 2018 - Politics of the Machines - Art and After", $xml->xpath('//core:result/core:content[1]/stab:title')[0]);
         $this->assertEquals("Change the Refrains; Eco-Logic Design for the Revaluation of Urban Space", $xml->xpath('//core:result/core:content[2]/stab:title')[0]);
@@ -38,7 +35,7 @@ class TestParsingPureWsRestResponse extends WP_UnitTestCase
 
     function test_parsing_of_author_first_names_should_work_from_valid_data()
     {
-        $xml = simplexml_load_file(FEEDURL);
+        $xml = simplexml_load_file(WSRESTFILE);
 
         $this->assertEquals(["Laura", "Morten"], $xml->xpath('//core:result/core:content[1]/stab:persons/person-template:personAssociation/person-template:name/core:firstName'));
         $this->assertEquals(["Jonas"], $xml->xpath('//core:result/core:content[2]/stab:persons/person-template:personAssociation/person-template:name/core:firstName'));
@@ -49,7 +46,7 @@ class TestParsingPureWsRestResponse extends WP_UnitTestCase
 
     function test_parsing_of_publication_years_should_work_from_valid_data()
     {
-        $xml = simplexml_load_file(FEEDURL);
+        $xml = simplexml_load_file(WSRESTFILE);
 
         $this->assertEquals(["2018", "2018", "2018", "2016", "2018"], $xml->xpath('//core:result/core:content/stab:publicationDate/core:year'));
     }
